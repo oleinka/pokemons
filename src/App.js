4dca97859b11f7ea6347ebb67437f39d6dce3f45
Marta <m.olejniczak550@gmail.com>
2020-04-06 22:57:46 +0200

    pokemon list component

--- src/App.js
+++ src/App.js
@@ -2,62 +2,34 @@ import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import './App.css';
 import Logo from './components/Logo/Logo';
+import PokemonList from './components/PokemonList/PokemonList';
+
 
 
 const App = () => {
-  const [pokemons, setPokemons] = useState([]);
   const [pokemon, setPokemon] = useState(null);
   const [nextLink, setNextLink] = useState(null);
   const [prevLink, setPrevLink] = useState(null);
   const [page, setPage] = useState(1);
-  const [url, setUrl] = useState('https://pokeapi.co/api/v2/pokemon?offset=0&limit=1000');
-
-  useEffect(() => {
-      axios.get(url)
-          .then((response) => {
-              setPokemons(response.data.results);
-          })
-          .catch(error => {
-              console.error(error)
-          })
-
-  }, [url]);
-
-  const getPokemonData = (url) => {
-      axios.get(url).then((response) => {
-          setPokemon(response.data);
-      })
-  };
-
+  
   return (
-      <>
-      <header>
-        <Logo/>
-      </header>
-      <div className='pokemonList'>
-          <ul>
-            {pokemons.filter((item, index) => {
-              return index < 8;
-              }).map(({name, url}) => (
-              <li>
-                <p>{name}</p>
-                <button onClick={() => {
-                  getPokemonData(url)
-                  }}>Pokaż w Pokedexie
-                </button>
-              </li>
-              ))}
-          </ul>
-      </div>
-
-          <button onClick={() => {
-              setPage(page - 1)
-          }}>Poprzednia strona
-          </button>
-          <button onClick={() => {
-              setPage(page + 1)
-          }}>Następna strona</button>
-      </>
+    <>
+    <header>
+    <Logo/>
+    </header>
+    <div className={'pokemonList'}>
+    <ul>
+    <PokemonList/>
+    </ul>
+    </div>
+    <button onClick={() => {
+        setPage(page - 1)
+    }}>Poprzednia strona
+    </button>
+    <button onClick={() => {
+        setPage(page + 1)
+    }}>Następna strona</button>
+    </>
   );
 }
 
