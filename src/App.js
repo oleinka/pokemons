30720e481875233160484207651ef27987d83279
Marta <m.olejniczak550@gmail.com>
2020-04-08 22:04:07 +0200

    change style of pokedex

--- src/App.js
+++ src/App.js
@@ -2,26 +2,51 @@ import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import './App.css';
 import Logo from './components/Logo/Logo';
-import PokemonList from './components/PokemonList/PokemonList';
-
-
 
 const App = () => {
   const [pokemon, setPokemon] = useState(null);
   const [nextLink, setNextLink] = useState(null);
   const [prevLink, setPrevLink] = useState(null);
   const [page, setPage] = useState(1);
+  const [pokemons, setPokemons] = useState([]);
+  const [url, setUrl] = useState('https://pokeapi.co/api/v2/pokemon/?offset=20&limit=1000');
+
+  useEffect(() => {
+      axios.get(url)
+          .then((response) => {
+              setPokemons(response.data.results);
+          })
+          .catch(error => {
+              console.error(error)
+          })
+
+  }, []);
+
+  const getPokemonData = (url) => {
+      axios.get(url).then((response) => {
+          setPokemons(response.data.results);
+      })
+  };
   
   return (
     <>
-    <header>
     <Logo/>
-    </header>
+    <div className={'pokemonDisplay'}>
+
+    </div>
     <div className={'pokemonList'}>
     <ul>
-    <PokemonList/>
+    {pokemons.map(({name}) => (
+      <li>
+      <p>{name}</p>
+      <button>Pokaż
+      </button>
+      </li>
+      ))}
     </ul>
     </div>
+
+
     <button onClick={() => {
         setPage(page - 1)
     }}>Poprzednia strona
@@ -34,3 +59,4 @@ const App = () => {
 }
 
 export default App;
+
