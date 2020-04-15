9a0b767e1a90282de0cded07bda2e4b24ca5608b
Marta <m.olejniczak550@gmail.com>
2020-04-15 22:37:03 +0200

    overflow scroll remove

--- src/components/PokemonDisplay/PokemonDisplay.js
+++ src/components/PokemonDisplay/PokemonDisplay.js
@@ -3,12 +3,17 @@ import styles from '../PokemonDisplay/PokemonDisplay.module.scss';
 
 const PokemonDisplay = ({pokemon}) => {
 
+const pokemonType = pokemon.types;
+
+
+
 return ( <>
 <div className={styles.screen}>
     <img className={styles.pokemonImg} src={pokemon.sprites.front_default} alt='pokemon'/>
     <div>
         <p>{pokemon.name}</p>
-        <p>Type: {pokemon.types[0].type.name}</p>
+        <p>Typ: {pokemonType[0].type.name}</p>
+        <p>Waga: {pokemon.weight}</p>
     </div>
 </div>
 </>
